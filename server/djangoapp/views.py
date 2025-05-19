c901d410e7223263ec62f52d1c42ef7d089ac2a2
vices1967 <victor.viceconti@alt3rlab.com>
2025-05-19 20:27:33 -0300

    x2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -144,3 +144,4 @@ def get_cars(request):
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
+    
